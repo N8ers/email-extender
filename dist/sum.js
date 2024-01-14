29a9253bf1c35d0ca667cfffa1a5384841bdb5e6
sheryak_n <nathanraysheryak@gmail.com>
2024-01-14 09:56:22 -0500

    remove the sum files

--- dist/sum.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sum = void 0;
-function sum(a) {
-    console.log("a ", a);
-    return 1;
-}
-exports.sum = sum;
-sum(5);
--- src/sum.ts
+++
@@ -1,6 +0,0 @@
-export function sum(a: number) {
-  console.log("a ", a)
-  return 1
-}
-
-sum(5)
